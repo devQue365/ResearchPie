036ee91a5296e3139d858616ecc9e81fcaa1ac3c
devQue365 <devanshr2810@gmail.com>
2026-01-26 18:04:53 +0530

    Updated TTS model for final orchestration

--- app/Functions/TTS/tts.py
+++ app/Functions/TTS/tts.py
@@ -1,4 +1,5 @@
 import os
+import nltk
 import torch
 import numpy as np
 import asyncio
@@ -10,7 +11,9 @@ load_dotenv(dotenv_path="orian-development/app/.env")
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-class Text_To_Speech:
+nltk.download("punkt", quiet=True)
+
+class OveroTTS:
     def __init__(self):
         # Sound Device Configs
         self.sample_rate = 22050
@@ -24,8 +27,10 @@ class Text_To_Speech:
             progress_bar=False,
         ).to(device)
 
-        # Streaming
-        self.queue = asyncio.Queue(maxsize=10)
+        # Streaming and Asynchronous Queues
+        self.audio_q = asyncio.Queue(maxsize=10)
+        self.sentence_q = asyncio.Queue()
+
         self.buffer_chunks = 4
         self.stream = sd.OutputStream(
             samplerate=self.sample_rate,
@@ -35,67 +40,77 @@ class Text_To_Speech:
         )
         self.stream.start() # Start the stream
     
-    async def audio_producer(self, sentence: str):
+    async def process_request(self, token_stream):
+
+        buffer = ""
+
+        async for token in token_stream:
+            buffer += token
+            sentences = nltk.sent_tokenize(buffer)
+
+            for sent_sentence in sentences[:-1]:
+                await self.sentence_q.put(sent_sentence.strip())
+        
+            buffer = sentences[-1]
+
+        if buffer.strip():
+            await self.sentence_q.put(buffer.strip())
+
+    async def audio_producer(self):
         """
         Generate and chunk audio tensor
         """
-        audio_tensor = self.model.tts(text = sentence, language = "en", speaker_wav=self.audio_prompt_path)
-        audio_tensor = np.asarray(audio_tensor, dtype=np.float32)
-        # Get the audio chunks
-        chunks = [
-            audio_tensor[i:i + self.chunk_size]
-            for i in range(0, len(audio_tensor), self.chunk_size)
-        ] 
 
-        # process each chunk -> Put in queue
-        for chunk in chunks:
-            await self.queue.put(chunk)
-        
-        # await self.queue.put(None) # Flag "End Of Signal"
+        while True:
+            # Get the sentence from sentence queue
+            sentence = await self.sentence_q.get()
+    
+            if sentence is None:
+                break
+
+            audio_tensor = self.model.tts(text = sentence, language = "en", speaker_wav=self.audio_prompt_path)
+            audio_tensor = np.asarray(audio_tensor, dtype=np.float32)
+            # Get the audio chunks
+            chunks = [
+                audio_tensor[i:i + self.chunk_size]
+                for i in range(0, len(audio_tensor), self.chunk_size)
+            ] 
+
+            # Process each chunk -> Put in queue
+            for chunk in chunks:
+                await self.audio_q.put(chunk)
+            
+            # Add a small pause between sentences
+            # pause = np.zeros(int(0.15 * self.sample_rate), dtype=np.float32)
+            # await self.audio_q.put(pause)
 
     async def audio_consumer(self):
         """
         Play audio
         """
 
-        silence = np.zeros(int(0.1 * 5), dtype=np.float32)
+        silence = np.zeros(int(0.1 * self.sample_rate), dtype=np.float32)
 
         # Wait for buffer to load with chunks
-        while(self.queue.qsize() < self.buffer_chunks):
+        while(self.audio_q.qsize() < self.buffer_chunks):
             await asyncio.sleep(0.003)
         
         # Playback
         while True:
             try:
-                chunk = await self.queue.get()
+                chunk = await self.audio_q.get()
             except asyncio.QueueEmpty:
                 self.stream.write(silence)
                 continue
             if chunk is None:
                 break
-
+            
             self.stream.write(chunk)
-            # await asyncio.sleep(self.chunk_ms / 1000)
-        self.close()
 
-    def close(self):
         self.stream.stop()
         self.stream.close()
 
-async def main():
-    tts = Text_To_Speech()
-    consumer = asyncio.create_task(tts.audio_consumer())
-    while True:
-        ip = await asyncio.to_thread(input, "You : ")
-
-        if ip.lower() in {'exit', 'stop'}:
-            break
+    async def shutdown(self):
+        await self.sentence_q.put(None)
+        await self.audio_q.put(None)
         
-        await tts.audio_producer(ip)
-
-    
-    await tts.queue.put(None)
-    await consumer
-
-if __name__ == '__main__':
-    asyncio.run(main())
