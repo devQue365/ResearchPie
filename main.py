da0f4f4cddc4d903d6ee20364172a3497d6ca235
devQue365 <devanshr2810@gmail.com>
2026-04-07 02:53:01 +0530

    Updated main

--- main.py
+++ main.py
@@ -1,119 +1,28 @@
-import time
-import random
-import threading
-import asyncio
-from torch.cuda import empty_cache
-from rich.live import Live
-from rich.layout import Layout
-from rich.panel import Panel
-from rich.spinner import Spinner
-from rich.console import Console
+import json
+import sys
+import os
+from PyQt5.QtWidgets import QApplication
 
-from app.Intelligence.chat_response import ResponseAI
-
-# Empty CUDA Cache
-empty_cache()
-
-# Initialize Console
-console = Console()
+from app_main import MainWindow
+from dotenv import load_dotenv
+# Import resources to make icons available
+import static.resource_rc
 
 
-def make_layout():
-    """Defines the terminal layout."""
-    layout = Layout()
-    # Split the screen into a fixed 'header' and a 'body' for jokes
-    layout.split_column(
-        Layout(name="header", size=3),
-        Layout(name="body", size=5)
-    )
-    return layout 
+load_dotenv(dotenv_path="./app/.env")
 
-class Session(object):
-    def __init__(self):
-        self.layout = None
-        self.message_cache = []
-        self.jokes_cache = []
-        self.env_loaded = False
-        self.has_replied = False
-        self.layout = make_layout()
-        self.intel = ResponseAI()
-        self.initialize_session()       
+if __name__ == '__main__':
+    flag = os.path.exists(os.getenv("DB_PATH"))
+    print(flag)
 
-    def initialize_session(self):
-        """
-        Initialize Orian Session
-        """
+    if not flag:
+        os.mkdir(os.getenv("DB_PATH"))
+        with open(os.getenv("DB_PATH") + "/data.json", "w") as f:
+            f.write(json.dumps([]))
 
-        def load_environment():
-            """
-            Load the model's work environment
-            """
-            self.intel.load()
-            self.env_loaded = True
+    app = QApplication(sys.argv)
 
-        
+    window = MainWindow()
+    window.show()
 
-        # Use threads to do the background work
-        thread = threading.Thread(target=load_environment)
-        thread.start()
-
-        # Load the messages
-        with open("orian-development/message_logs.txt", 'r') as fh:
-            self.message_cache = fh.readlines()
-            random.shuffle(self.message_cache)
-
-        # Load jokes
-        with open("orian-development/jokes.txt", 'r') as fh:
-            self.jokes_cache = fh.readlines()
-            random.shuffle(self.jokes_cache)
-        
-        # Set up the welcome message
-        welcome_message = Panel(
-            Spinner("balloon", text=f"[bold cyan]{self.message_cache[random.randint(0, 100)]}[/]", style="bold green"),
-            border_style="blue",
-        )
-
-        self.layout["header"].update(welcome_message)
-
-        # Start the live display
-        with Live(self.layout, refresh_per_second=4, screen=False, transient=True):
-            i = 0
-            while not self.env_loaded:
-                # Update only the body section
-                self.layout["body"].update(
-                    Panel(
-                        Spinner("flip", f"[bold yellow]{self.jokes_cache[i]}[/]", style="bold yellow"), 
-                        title="[bold]Knock Knock ?[/]", 
-                        border_style="magenta",
-                        padding=(1, 2)
-                    )
-                )
-                
-                # Wait before switching to the next joke
-                time.sleep(10)
-                i += 1
-
-            self.layout["header"].update(
-                Panel("Orian is online")
-            )
-        thread.join()
-        asyncio.create_task(self.intel.tts_worker())
-
-        
-async def main():
-    """
-    The main CLI based chat interface
-    """
-
-    # Initialize Session
-    session = Session()
-
-    # Initialize TTS worker
-    asyncio.create_task(session.intel.tts_worker())
-
-    while True:
-        prompt = await asyncio.to_thread(input, "\nYou : ")
-        response = await session.intel.serve_query(prompt)
-        console.print(response)
-
-asyncio.run(main())
+    sys.exit(app.exec())
